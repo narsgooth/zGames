c55e6a06d22f32c549c2902cc80cf5ae35760347
narsgooth <l4me.duck@gmail.com>
2025-08-31 16:10:07 -0500

    add

--- js/genres.js
+++ js/genres.js
@@ -1,7 +1,16 @@
 var genres = [
+'adventure',
 'arcade',
+'building',
+'educational',
+'fps',
+'platform',
+'multiplayer',
+'open-world',
+'puzzle',
+'racing',
+'rhythm',
+'side-scroller',
+'strategy',
+'sports'
 ];
-
-/*
-'arcade',
-*/
